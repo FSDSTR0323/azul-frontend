b807bd85ae4cb426f3e73ac496c2df15378730dc
crobledor <crobledoramos@gmail.com>
2023-06-19 13:42:23 +0200

    profile page updated, index.js updated deleting React strict mode

--- src/components/containers/perfil/UserData.js
+++ src/components/containers/perfil/UserData.js
@@ -1,134 +1,214 @@
+import { useState, useEffect } from 'react';
+import { Controller, useForm } from "react-hook-form"
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Divider from '@mui/material/Divider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import dayjs from 'dayjs';
+import axios from 'axios';
+import { authorizationConfig } from '../../../security';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-// import { useForm } from "react-hook-form"
-// import TextField from '@mui/material/TextField';
-// import Box from '@mui/material/Box';
-// import axios from 'axios';
-// import { useState, useContext } from 'react';
-// import { UserContext } from '../../../contexts/UserContext';
-// import BannerBackground from "../../../assets/home-banner-background.png";
-// import Divider from '@mui/material/Divider';
-
-
-// function data() {
+export const UserData = ({ name }) => {
     
-//     const [count, setCount] = useState(0);
-  
-//     return (
-//       <div>
-//         <p>You clicked {count} times</p>
-//         <button onClick={() => setCount(count + 1)}>
-//           Click me
-//         </button>
-//       </div>
-//     );
-//   }
-
-// export const UserData = () => {
-//     const { token, setToken } = useContext(UserContext)
-//     const { register, handleSubmit, formState: { errors } } = useForm();
-//     const [ error, setError ] = useState({})
-
-//     const onSubmit = async (formData, e) =>  {
-//         try {
-//             const res = await axios.post('http://localhost:5000/register', formData)
-//             setToken(res.data.token)
-//             console.log(res.data)
-//             window.localStorage.setItem('token', token)
-//         } catch(err) {
-//             let mensajeError = {}
-//             mensajeError = err.response.data
-//             console.log("el mensaje de error es", mensajeError)
-//             setError({
-//                 error: err.response.data.error,
-//             })
-//             console.log("el error asignado es", error)
-//             setTimeout(() => {
-//                 setError({})
-//             }, 3000)
-//         }
-//     }
-//     return (
-//         <>
-//         {/* //     <div className="home-bannerImage-container">
-//         //         <img src={BannerBackground} alt="" />
-//         //     </div> */}
-
-// <div className="home-bannerImage-container">
-//         <img id="logo" src={BannerBackground} alt="" />
-//       </div>
-//             <Box onSubmit={handleSubmit(onSubmit)}
-//                 className="form-box"
-//                 component="form"
-//                 sx={{
-//                     '& > :not(style)': { m: 1, width: '100%' },
-//                 }}
-//                 noValidate
-//                 autoComplete="off"
-//             >
-//                 <div className="registration-form-box">
-//                     <div id="details">
-//                         <TextField
-//                             id="outlined-controlled"
-//                             label="Nombre"
-//                             {...register("name")}
-
-//                         /> 
-                        
-//                         <TextField
-//                             id="outlined-controlled"
-//                             label="Apellido"
-//                             {...register("surname")}
-//                         />
-//                         <TextField
-//                             id="outlined-controlled"
-//                             label="Fecha de Nacimiento"
-//                             {...register("birthdate")}
-//                         />   
-//                         <TextField
-//                             id="outlined-controlled"
-//                             label="Dirección"
-//                             {...register("address")}
-//                         />  
-//                         <TextField
-//                             id="outlined-controlled"
-//                             label="Email"
-//                             {...register("email")}
-//                         /> 
-//                         <TextField
-//                             id="outlined-controlled"
-//                             label="Teléfono"
-//                             {...register("phone")}
-//                         /> 
-//                     </div>
-//                     <Divider style={{width:'60%', maxWidth:"40rem"}} />
-//                     <div id="credentials">
-//                         <TextField
-//                             id="outlined-controlled"
-//                             label="Usuario"
-//                             {...register("username")}
-//                         />
-//                         <TextField
-//                             id="outlined-controlled"
-//                             label="Contraseña"
-//                             type="password"
-//                             button = 'editar'
-//                         /> 
-//                       <TextField
-//                             id="outlined-controlled"
-//                             label="Teléfono"
-//                             {...register("phone")}
-//                         /> 
-                 
-//                     </div>
-//                 </div> 
-                
-//                 <button className="secondary-button" id='login-form-box-button'> editar perfil</button>       
-                 
-//                 {/* {error && <p>{error}</p>} */}
-//             </Box>
-//         </>
-//     )
-// }
+  const [userData, setUserData] = useState("")
+  const [formDisabled, setFormDisabled] = useState(true)
+  const [ error, setError ] = useState()
+  const { control, register, handleSubmit, getValues, reset, formState: { errors } } = useForm();
 
 
+    useEffect(() => {
+        (async() => {
+            try{
+            const userDataRes = await axios.get("http://localhost:5000/profile", authorizationConfig)
+            console.log("La data res es:", userDataRes)
+            setUserData(userDataRes)
+            reset({
+                name: userDataRes.data.name,
+                surname: userDataRes.data.surname ,
+                birthdate: dayjs(userDataRes.data.birthdate),
+                address: userDataRes.data.address,
+                email: userDataRes.data.email,
+                phone: userDataRes.data.phone,
+                username: userDataRes.data.username,
+            })
+            } catch(error){
+                console.log(error)
+            }
+        })()
+    }, [])
+
+    const handleFormBox = () => {
+        setFormDisabled(!formDisabled)
+    }
+
+    const onSubmit = async (formData) =>  {
+        setError(false);
+        console.log("Llegamos aquí")
+        console.log("la data aactualizada es", formData)
+        try {
+            const modifiedDataRes = await axios.put('http://localhost:5000/profile', formData, authorizationConfig)
+            toast.success(`Has modificado los datos correctamente`, {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+            });
+            console.log(modifiedDataRes)
+            setUserData(modifiedDataRes)
+            reset({
+                name: modifiedDataRes.data.name,
+                surname: modifiedDataRes.data.surname ,
+                birthdate: dayjs(modifiedDataRes.data.birthdate),
+                address: modifiedDataRes.data.address,
+                email: modifiedDataRes.data.email,
+                phone: modifiedDataRes.data.phone,
+                username: modifiedDataRes.data.username,
+            })
+            setFormDisabled(!formDisabled)
+        }  
+        catch(err) {
+            setError(err.response.data.error)
+            setTimeout(() => {
+                setError()
+                console.log(error)
+            }, 3000)
+        }
+    }
+
+    if(userData) {
+        return (
+            <>
+                <Box onSubmit={handleSubmit(onSubmit)}
+                className="form-box"
+                component="form"
+                sx={{
+                    '& > :not(style)': { m: 1, width: '100%' },
+                }}
+                noValidate
+                autoComplete="off"
+                >
+                    <div className="profile-form-box">
+                        <div id="details">
+                            <div id="details-child">
+                                <TextField
+                                    id="outlined-controlled"
+                                    label="Nombre *"
+                                    defaultValue={userData.data.name}
+                                    disabled={formDisabled}
+                                    {...register("name", { required: true })}
+                                /> 
+                                <TextField
+                                    id="outlined-controlled"
+                                    label="Apellido"
+                                    defaultValue={userData.data.surname}
+                                    disabled={formDisabled}                    
+                                    {...register("surname")}
+                                /> 
+                            </div>
+                            <div id="details-child"> 
+                                <Controller
+                                    name="birthdate"
+                                    control={control}
+                                    render={({ field }) => <LocalizationProvider
+                                    dateAdapter={AdapterDayjs}
+                                    >
+                                    <DatePicker 
+                                    label={`Fecha nacimiento *`}
+                                    defaultValue={dayjs(userData.data.birthdate)}
+                                    disabled={formDisabled}
+                                    {...field}
+                                    slotProps={{
+                                        textField: {
+                                            error:false,
+                                        },
+                                    }}
+                                    />
+                                    </LocalizationProvider>
+                                    }
+                                />
+                                {errors.birthdate && <span className="details-error-message">La fecha de nacimiento es requerida</span>}
+                                <TextField
+                                    id="outlined-controlled"
+                                    label="Dirección"
+                                    defaultValue={userData.data.address}
+                                    disabled={formDisabled}
+                                    {...register("address")}
+                                /> 
+                            </div>
+                            <div id="details-child">
+                                <TextField
+                                    id="outlined-controlled"
+                                    label="Email*"
+                                    defaultValue={userData.data.email}
+                                    disabled={formDisabled}
+                                    {...register("email", {
+                                        required: true,
+                                        pattern: {
+                                            value: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+                                            message: "Introduzca un email válido"
+                                        }
+                                    })}
+                                /> 
+                                {errors.email?.type === 'pattern' && <span className="details-error-message error-messages">{errors.email.message}</span>} 
+                                <TextField
+                                    id="outlined-controlled"
+                                    label="Teléfono"
+                                    defaultValue={userData.data.phone}
+                                    disabled={formDisabled}
+                                    {...register("phone")}
+                                /> 
+                            </div>
+                        </div>
+                        <Divider style={{width:'60%', maxWidth:"40rem"}} />
+                        <div id="credentials">
+                            <div id ="credentials-child">
+                                <TextField
+                                    id="outlined-controlled"
+                                    label="Usuario*"
+                                    defaultValue={userData.data.username}
+                                    disabled={formDisabled}
+                                    {...register("username", { required: true})}
+                                />
+                            </div>
+                            {/* <div id ="credentials-child">
+                                <TextField
+                                    id="outlined-controlled"
+                                    label="Contraseña*"
+                                    defaultValue="password"
+                                    disabled={formDisabled}
+                                    {...register("password", { required: true})}
+                                    type="password"
+                                />
+                                {errors.passwordConfirmation?.type === 'validate' && <span className="details-error-message error-messages">Las contraseñas no coinciden</span>}
+                            </div> */}
+                            {/* <div id ="credentials-child">
+                                <TextField
+                                    id="outlined-controlled"
+                                    label="Confirmación*"
+                                    {...register("passwordConfirmation", { 
+                                        required: true,
+                                        validate: value => value === getValues('password')
+                                        }
+                                    )}
+                                    type="password"
+                                />
+                            </div> */}
+                        </div>
+                        {(errors.email?.type === 'required' || errors.name?.type === 'required' || errors.username?.type === 'required' || errors.password?.type === 'required' || errors.passwordConfirmation?.type === 'required') && <span className='error-messages' style={{ display:'inline-block', textAlign:'center', marginTop: "2rem"}}>Debe rellenar todos los campos obligatorios (*)</span>}
+                        {!formDisabled && <button className="secondary-button" id='login-form-box-button'>Aplicar cambios</button>}       
+                    </div> 
+                </Box>
+                {formDisabled && <button className="secondary-button" id='login-form-box-button' style={{margin: "0 auto"}} onClick={handleFormBox}>Modificar</button>}
+            </> 
+        );
+    }
+}
 
